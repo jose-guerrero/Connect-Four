7286726e3d80014eaed3be93d937355c2c717268
jose-guerrero <jose.guerrerog@outlook.com>
2018-01-31 15:17:34 +1100

    connect-four

--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,3 @@
-/*!
- * jQuery JavaScript Library v1.12.1
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright jQuery Foundation and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2016-02-22T19:07Z
- */
 
 (function( global, factory ) {
 
